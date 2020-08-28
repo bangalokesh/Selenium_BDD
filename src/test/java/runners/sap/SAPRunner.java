57e3ebccd7963ed05eedc650e1f26bd2044fb338
Lokesh Banga <bangalokesh@gmail.com>
2020-08-27 22:57:06 -0400

    updated feature files path

--- src/test/java/runners/sap/SAPRunner.java
+++ src/test/java/runners/sap/SAPRunner.java
@@ -12,7 +12,7 @@ import pageclasses.BasePage;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		tags= "@sapFileTest",
-		features= "\\src\\test\\resources\\features\\",
+		features= "src/test/resources/features/",
 		glue="stepdefinition",
 		monochrome = true,
 		plugin = { "pretty", "html:reports/cucumber-reports", "json:reports/cucumber-reports/Cucumber.json", "junit:reports/cucumber-reports/Cucumber.xml", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:reports/cucumber-reports/report.html" }
